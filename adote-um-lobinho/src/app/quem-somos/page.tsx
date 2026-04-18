17d3608706b1c1a62fdd9f509450a0d88789e106
Augusto Brigida Silva <augusto.silva@injunior.com.br>
2026-04-18 15:55:16 -0300

    feat: implmentando página de quem somos

---
+++ adote-um-lobinho/src/app/quem-somos/page.tsx
@@ -0,0 +1,14 @@
+import style from './styles.module.css'
+
+export default function QuemSomos() {
+    return (
+        <section className={style.who}>
+
+            <h1 className={style.whot}>Quem somos</h1>
+            <p className={style.whop}>
+             Gingerbread cookie gingerbread jujubes cake biscuit tart fruitcake. Jujubes chupa chups wafer cheesecake lemon drops. Pastry caramels cookie marshmallow. Chupa chups jelly beans apple pie powder marzipan oat cake jelly-o chupa chups biscuit. Lollipop biscuit jujubes tootsie roll oat cake chocolate bar. 
+            </p>
+
+        </section>
+    )
+}
